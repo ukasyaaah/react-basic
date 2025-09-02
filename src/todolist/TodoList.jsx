b1a1bf7f7331ea4c36a905b797bf11882af707f2
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-02 13:41:50 +0700

    Nambah key pada component yg mau di render

--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -2,47 +2,26 @@ import TodoConditional, { TodoAndLogic, TodoTernary } from "./Todo";
 
 export default function TodoList() {
   const data = [
-    {
-      text: "Belajar HTML",
-      isCompleted: true,
-      isDeleted: true,
-    },
-    {
-      text: "Belajar CSS",
-      isCompleted: true,
-    },
-    {
-      text: "Belajar Tailwind",
-      isCompleted: true,
-    },
-    {
-      text: "Belajar JS",
-      isCompleted: true,
-    },
-    {
-      text: "Belajar Vue",
-      isCompleted: false,
-    },
-    {
-      text: "Belajar Next.js",
-      isCompleted: false,
-    },
+    { id: 0, text: "Belajar HTML", isCompleted: true, isDeleted: true },
+    { id: 1, text: "Belajar CSS", isCompleted: true },
+    { id: 2, text: "Belajar Tailwind", isCompleted: true },
+    { id: 3, text: "Belajar JS", isCompleted: true },
+    { id: 4, text: "Belajar Vue", isCompleted: false },
+    { id: 5, text: "Belajar Next.js", isCompleted: false },
   ];
 
-  const todosC = data.map((todo) => {
-    return <TodoConditional {...todo} />;
-  });
+  // Gunakan key pada setiap komponen yg mau di render
+  const todosC = data.map((todo) => (
+    <TodoConditional key={todo.id} {...todo} />
+  ));
 
-  const todosL = data.map((todo) => {
-    return <TodoAndLogic {...todo} />;
-  });
+  const todosL = data.map((todo) => <TodoAndLogic key={todo.id} {...todo} />);
 
-  const todosT = data.map((todo) => {
-    return <TodoTernary {...todo} />;
-  });
+  const todosT = data.map((todo) => <TodoTernary key={todo.id} {...todo} />);
 
   return (
     <>
+      {/* Biasanya, user react langsung mapping disini (gaperlu bikin variable TodosC, TodosT) */}
       <section>
         <h1 className="title">Todo Conditional</h1>
         <ul> {todosC} </ul>
