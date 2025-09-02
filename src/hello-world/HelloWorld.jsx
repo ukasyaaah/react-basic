1b061dbe110d5d2b22ddf55e33d5f9b0d5f2d269
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-02 10:24:21 +0700

    style

--- src/hello-world/HelloWorld.jsx
+++ src/hello-world/HelloWorld.jsx
@@ -6,3 +6,36 @@ export default function HelloWorld() {
     </>
   );
 }
+
+export function HeaderHelloWorld() {
+  const text = "Hello World";
+  return (
+    <>
+      <h1
+        style={{
+          color: "aquamarine",
+          backgroundColor: "blue",
+          fontFamily: "sans-serif",
+        }}
+      >
+        {text}
+      </h1>
+    </>
+  );
+}
+export function ParagraphHelloWorld() {
+  
+  const text = "Selamat Belajar ReactJS dari Ukhasyah";
+  const style = {
+    fontStyle: "italic",
+    fontFamily: "sans-serif",
+  };
+
+
+  return (
+    <>
+      {/* Mengakses kode JS dgn kurawal */}
+      <p style={style}>{text} </p>
+    </>
+  );
+}
--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import HelloWorld from "./HelloWorld";
+import HelloWorld, { HeaderHelloWorld, ParagraphHelloWorld } from "./HelloWorld";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelloWorld />
     <HelloWorld />
     <HelloWorld />
+    <HeaderHelloWorld/>
+    <ParagraphHelloWorld/>
   </StrictMode>
 );
