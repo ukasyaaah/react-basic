8a83b2688190ba2cd25501c4b61954d0ed2a9eac
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-03 18:22:15 +0700

    Integrasi State & Context

--- src/profile/ProfileApp.jsx
+++ src/profile/ProfileApp.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import Profile from "./Profile";
 import ProfileAddress from "./ProfileAddress";
 import { ProfileContext } from "./ProfileContext";
+import ProfileForm from "./ProfileForm";
 
 export default function ProfileApp() {
+  const [name, setName] = useState("Ukhasyah");
   return (
     <>
-      <ProfileContext.Provider value="Ukhasyah">
+      <ProfileContext.Provider value={name}>
         <h1>Profile App</h1>
+        <ProfileForm name={name} setName={setName} />
         <Profile />
         <ProfileAddress />
       </ProfileContext.Provider>
