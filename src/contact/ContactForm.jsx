fd6bb0a989a1e41374dbb7d86155f09ed9ea5e4d
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-03 09:19:29 +0700

    object dlm state dgn immerjs

--- src/contact/ContactForm.jsx
+++ src/contact/ContactForm.jsx
@@ -1,6 +1,51 @@
 import { useState } from "react";
+import { useImmer } from "use-immer";
 
-export default function ContactForm() {
+// With Immer
+export default function ContactPerson() {
+  let [contact, setContact] = useImmer({
+    name: "",
+    message: "",
+  });
+
+  function handleNameChange(e) {
+    setContact((contact) => {
+      contact.name = e.target.value;
+    });
+  }
+
+  function handleMessageChange(e) {
+    setContact((contact) => {
+      contact.message = e.target.value;
+    });
+  }
+
+  return (
+    <section>
+      <h1>Contact Person Form</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="Name"
+          value={contact.name}
+          onChange={handleNameChange}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Message"
+          value={contact.message}
+          onChange={handleMessageChange}
+        />
+      </form>
+      <h1>Contact Person</h1>
+      <p>Name : {contact.name} </p>
+      <p>Message : {contact.message} </p>
+    </section>
+  );
+}
+
+export function ContactForm() {
   const [contact, setContact] = useState({
     name: "",
     message: "",
--- src/contact/main.jsx
+++ src/contact/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Container from "../hello-world/Container";
-import ContactForm from "./ContactForm";
+import ContactPerson, { ContactForm } from "./ContactForm";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Container>
       <ContactForm />
+      <ContactPerson />
     </Container>
   </StrictMode>
 );
