41c3515a8902b4da41f7be647608349f0534e1ad
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-03 18:46:22 +0700

    Ref

--- src/button/AlertButton.jsx
+++ src/button/AlertButton.jsx
@@ -1,8 +1,11 @@
+import { useRef } from "react";
+
 export default function AlertButton({ text, textWhenButtonClicked }) {
+  const counter = useRef(0);
   function handleClick(e) {
     // e -> event object
     console.info(e.target);
-    return alert(textWhenButtonClicked);
+    return alert(`${textWhenButtonClicked} ${counter.current++}`);
   }
 
   return <button onClick={handleClick}> {text} </button>;
