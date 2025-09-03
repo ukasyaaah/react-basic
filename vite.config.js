ad1b064b3286998efb3da91a0d19d0e14d8d1f20
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-03 17:24:15 +0700

    Praktek Context

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
         task: "/task.html",
         counter: "/counter.html",
         note: "/note.html",
+        profile: "/profile.html",
       },
     },
   },
