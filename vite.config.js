f391eb2ba7c8261a5c3021fecd9206111d93b034
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-09-04 08:04:35 +0700

    Custom Hooks

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
         timer: "/timer.html",
         guestbook: "/guestbook.html",
         product: "/product.html",
+        online: "/online.html",
       },
     },
   },
